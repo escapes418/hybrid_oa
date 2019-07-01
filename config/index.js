eda18ddbe4a1623eced530ec850d09760686a0ee
shehaoyuan <shehy@sijibao.com>
2019-07-01 16:12:38 +0800

    1

--- config/index.js
+++ config/index.js
@@ -34,7 +34,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: '192.168.27.189', // can be overwritten by process.env.HOST
-    port: 7778, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 7777, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
