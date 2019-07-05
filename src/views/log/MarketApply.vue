7f59fba25fcebf945b44c1e1e96576746cc0065b
shehaoyuan <shehy@sijibao.com>
2019-07-05 15:57:37 +0800

    去掉抄送人红点

--- src/views/log/MarketApply.vue
+++ src/views/log/MarketApply.vue
@@ -3,10 +3,21 @@
     <group title="基本信息" label-margin-right="1em">
       <x-input title="当日电话拜访" v-model="applyData.custPhotoCount" readonly></x-input>
       <x-input title="当日上门拜访" v-model="applyData.custVisitCount" readonly></x-input>
-      <x-input title="当日意向数量" placeholder="请输入" type="number" v-model.trim="applyData.custIntentionCount">
+      <x-input
+        title="当日意向数量"
+        placeholder="请输入"
+        type="number"
+        v-model.trim="applyData.custIntentionCount"
+      >
         <span slot="label">当日意向数量<span class="required">*</span></span>
       </x-input>
-      <x-input title="当日签约数量" :max="50" placeholder="请输入" type="number" v-model.trim="applyData.custSignCount">
+      <x-input
+        title="当日签约数量"
+        :max="50"
+        placeholder="请输入"
+        type="number"
+        v-model.trim="applyData.custSignCount"
+      >
         <span slot="label">当日签约数量<span class="required">*</span></span>
       </x-input>
       <x-input
@@ -25,9 +36,16 @@
         v-model.trim="applyData.afterDailyPlan"
         :max="1000"
       >
-        <span slot="label" style="padding-right:10px;">明日工作计划<span class="required">*</span></span>
+        <span slot="label" style="padding-right:10px;"
+          >明日工作计划<span class="required">*</span></span
+        >
       </x-textarea>
-      <x-textarea title="备注" :autosize="true" v-model.trim="applyData.remarks" :max="1000"></x-textarea>
+      <x-textarea
+        title="备注"
+        :autosize="true"
+        v-model.trim="applyData.remarks"
+        :max="1000"
+      ></x-textarea>
     </group>
 
     <group
@@ -54,7 +72,12 @@
       ></RemoteSearch>
       <x-input title="拜访类型" v-model="item.visitTypeName" readonly></x-input>
       <x-input title="维护时间" v-model="item.custMaintenanceDateTime" readonly></x-input>
-      <x-textarea title="维护内容" :autosize="true" v-model="item.custMaintenanceContent" readonly></x-textarea>
+      <x-textarea
+        title="维护内容"
+        :autosize="true"
+        v-model="item.custMaintenanceContent"
+        readonly
+      ></x-textarea>
     </group>
     <div class="add-btn">
       <span class="pointer" @click="addItem">新增客户情况</span>
@@ -88,7 +111,6 @@
       ></multree>
       <multree
         sTitle="抄送对象"
-        :isRequired="true"
         :isPeople="true"
         sPlaceholder="请选择"
         :dataList="copyToPeoList"
@@ -106,14 +128,14 @@
   </div>
 </template>
 <script>
-import Vue from "vue";
-import com from "@/assets/js/common";
-import sinSelector from "@/components/sinSelector";
-import mulSelectors from "@/components/mulSelectors";
-import RemoteSearch from "@/components/RemoteSearch.vue";
-import multree from "@/components/multree.vue";
-import api from "@/assets/api/index.api";
-import Utils from "./utils";
+import Vue from 'vue';
+import com from '@/assets/js/common';
+import sinSelector from '@/components/sinSelector';
+import mulSelectors from '@/components/mulSelectors';
+import RemoteSearch from '@/components/RemoteSearch.vue';
+import multree from '@/components/multree.vue';
+import api from '@/assets/api/index.api';
+import Utils from './utils';
 import {
   Tab,
   TabItem,
@@ -134,11 +156,11 @@ import {
   Confirm,
   FlexboxItem,
   TransferDomDirective as TransferDom
-} from "vux";
-import { resolve } from "path";
+} from 'vux';
+import { resolve } from 'path';
 
 export default {
-  name: "LogMarketApply",
+  name: 'LogMarketApply',
   directives: {
     TransferDom
   },
@@ -170,23 +192,23 @@ export default {
     return {
       isReady: false,
       applyData: {
-        afterDailyPlan: "", //明日计划
+        afterDailyPlan: '', //明日计划
         afterVisitCount: undefined, //明日拜访客户数量
         custIntentionCount: undefined, //当日意向客户数量
         custPhotoCount: 0, //当日电话拜访数量
         custSignCount: undefined, //当日签约数量
         custVisitCount: 0, //当日上门拜访数量
-        remarks: "",
+        remarks: '',
         dailyCustMaintenanceList: [
           {
-            custId: "", //客户id ,
-            custMaintenanceContent: "", //维护内容
-            custMaintenanceDate: "", //维护时间
-            custMaintenanceDateTime: "",
-            custName: "", //客户name
-            visitType: "", //拜访类型
-            visitTypeName: "",
-            custLabel: ""
+            custId: '', //客户id ,
+            custMaintenanceContent: '', //维护内容
+            custMaintenanceDate: '', //维护时间
+            custMaintenanceDateTime: '',
+            custName: '', //客户name
+            visitType: '', //拜访类型
+            visitTypeName: '',
+            custLabel: ''
           }
         ],
         sendToList: [],
@@ -195,9 +217,9 @@ export default {
       custOption: {
         api: api.queryDailyCustMaintenance,
         params: {},
-        search: "custName",
-        key: "custId",
-        value: "custName"
+        search: 'custName',
+        key: 'custId',
+        value: 'custName'
       },
       sendToList: [],
       saveTimer: false,
@@ -226,28 +248,28 @@ export default {
     initAutoSave() {
       var _this = this;
       this.getAutoSaveTime().then(getData => {
-        com.comGetStorage("autoSave").then(rtn => {
+        com.comGetStorage('autoSave').then(rtn => {
           if (rtn.LogMarketApply !== undefined) _this.applyData = rtn.LogMarketApply; //给表单赋值
           _this.isReady = true;
           _this.saveTimer = setInterval(() => {
             rtn.LogMarketApply = _this.applyData;
-            com.comSetStorage("autoSave", rtn);
+            com.comSetStorage('autoSave', rtn);
           }, 10000);
         });
       });
     },
     getAutoSaveTime() {
       return new Promise((resolve, reject) => {
-        com.comGetStorage("queryDict").then(data => {
+        com.comGetStorage('queryDict').then(data => {
           var getData = data.dict;
           let temp = 10000;
           for (var i = 0; i < getData.length; i++) {
-            if (getData[i].type == "auto_save") {
+            if (getData[i].type == 'auto_save') {
               temp = parseInt(getData[i].value);
               break;
             }
           }
-          if (typeof temp == "number") {
+          if (typeof temp == 'number') {
             resolve(temp);
           } else {
             resolve(10000);
@@ -256,7 +278,7 @@ export default {
       });
     },
     getTreeList() {
-      com.covertHttp(api.orgAndUserInfo, { queryType: "2" }).then(rtn => {
+      com.covertHttp(api.orgAndUserInfo, { queryType: '2' }).then(rtn => {
         var list = com.addTreePeopel(rtn.data);
         var peopelList = com.hasChildren(list);
         this.copyToPeoList = com.clone(peopelList);
@@ -311,8 +333,8 @@ export default {
       if (!Utils.valiMarketApply(this)) return; //校验
       this.isSubmit = true;
       this.$vux.confirm.show({
-        title: "是否提交",
-        content: "确定执行操作？",
+        title: '是否提交',
+        content: '确定执行操作？',
         onCancel() {
           _this.isSubmit = false;
         },
@@ -339,21 +361,23 @@ export default {
     },
     delAutoSave() {
       clearInterval(this.saveTimer);
-      com.comGetStorage("autoSave").then(rtn => {
+      com.comGetStorage('autoSave').then(rtn => {
         delete rtn.LogMarketApply;
-        com.comSetStorage("autoSave", rtn);
+        com.comSetStorage('autoSave', rtn);
       });
     },
     backStep() {
       this.$router.go(-1);
     },
     changeCust(e) {
-      if (this.applyData.dailyCustMaintenanceList[e.itemIndex].custId == "") return;
-      this.applyData.dailyCustMaintenanceList[e.itemIndex].custMaintenanceContent = e.custMaintenanceContent;
-      this.applyData.dailyCustMaintenanceList[e.itemIndex].custMaintenanceDate = e.custMaintenanceDate;
-      this.applyData.dailyCustMaintenanceList[e.itemIndex].custMaintenanceDateTime = com.formatFullDateTime(
-        e.custMaintenanceDate
-      );
+      if (this.applyData.dailyCustMaintenanceList[e.itemIndex].custId == '') return;
+      this.applyData.dailyCustMaintenanceList[e.itemIndex].custMaintenanceContent =
+        e.custMaintenanceContent;
+      this.applyData.dailyCustMaintenanceList[e.itemIndex].custMaintenanceDate =
+        e.custMaintenanceDate;
+      this.applyData.dailyCustMaintenanceList[
+        e.itemIndex
+      ].custMaintenanceDateTime = com.formatFullDateTime(e.custMaintenanceDate);
       this.applyData.dailyCustMaintenanceList[e.itemIndex].custName = e.custName;
       this.applyData.dailyCustMaintenanceList[e.itemIndex].visitType = e.visitType;
       this.applyData.dailyCustMaintenanceList[e.itemIndex].visitTypeName = e.visitTypeName;
@@ -373,14 +397,14 @@ export default {
     },
     addItem() {
       this.applyData.dailyCustMaintenanceList.push({
-        custId: "", //客户id ,
-        custMaintenanceContent: "", //维护内容
-        custMaintenanceDate: "", //维护时间
-        custMaintenanceDateTime: "",
-        custName: "", //客户name
-        visitType: "", //拜访类型
-        visitTypeName: "",
-        custLabel: ""
+        custId: '', //客户id ,
+        custMaintenanceContent: '', //维护内容
+        custMaintenanceDate: '', //维护时间
+        custMaintenanceDateTime: '',
+        custName: '', //客户name
+        visitType: '', //拜访类型
+        visitTypeName: '',
+        custLabel: ''
       });
     },
     delItem(index) {
@@ -390,8 +414,8 @@ export default {
       //     return;
       // }
       this.$vux.confirm.show({
-        title: "是否删除",
-        content: "确定执行操作？",
+        title: '是否删除',
+        content: '确定执行操作？',
         onCancel() {},
         onConfirm() {
           _this.applyData.dailyCustMaintenanceList.splice(index, 1);
@@ -403,9 +427,9 @@ export default {
 };
 </script>
 <style lang="less">
-@import "../../assets/css/common/reset.fix.less";
-@import "../../assets/css/common/base.less";
-@import "../../assets/css/index.less";
+@import '../../assets/css/common/reset.fix.less';
+@import '../../assets/css/common/base.less';
+@import '../../assets/css/index.less';
 .fixbtn {
   width: 100%;
   position: fixed;
